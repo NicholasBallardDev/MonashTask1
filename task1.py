cc7a233a94e322a0608206e1fd52010fb94ba852
NicholasBallardDev <123438677+NicholasBallardDev@users.noreply.github.com>
2023-12-12 13:25:31 -0700

    Add files via upload

--- task1.py
+++ task1.py
@@ -38,31 +38,37 @@ def find_integer(lst):
 
 #Test Cases
 #In a Perfect Scenario
+#input:[8,1,5,3]
 #expected: 8153
 res = find_integer([8,1,5,3])
 print("test 1:",type(res), res)
 
 #When List is Empty
+#input:[]
 #expected: None
 res = find_integer([])
 print("test 2:",type(res), res)
 
 #When a string is in the list
+#input:[8,"hi",2,4,5]
 #expected: None
 res = find_integer([8,"hi",2,4,5])
 print("test 3:",type(res), res)
 
 #When a string is in the list
+#input:[8,"0",2,4,5]
 #expected: 80245
 res = find_integer([8,"0",2,4,5])
 print("test 3:",type(res), res)
 
 #When 0's are at the front of the list
+#input:[0,0,0,5]
 #expected: 5
 res = find_integer([0,0,0,5])
 print("test 4:",type(res), res)
 
 #When the input is not a list
+#input:True
 #expected: None
 res = find_integer(True)
-print("test 4:",type(res), res)
+print("test 5:",type(res), res)
